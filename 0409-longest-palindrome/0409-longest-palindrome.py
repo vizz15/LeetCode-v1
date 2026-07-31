1b38017e0825a8820241b1f300e281496e9dd26d
Vinay Gowda C <vinaygowda1509@gmail.com>
2026-08-01 00:49:30 +0530

    Time: 4 ms (15.37%), Space: 19.3 MB (28.21%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,21 +1,19 @@
-from collections import Counter
-
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        counts = Counter(s)
-        length = 0
-        has_odd = False
-        
-        for count in counts.values():
-            # Add all possible pairs for this character
-            length += (count // 2) * 2
-            # Check if there is a remainder to use as a center
-            if count % 2 == 1:
-                has_odd = True
-                
-        # If at least one character had an odd count, 
-        # we can place it in the unique center of the palindrome
-        if has_odd:
-            length += 1
-            
-        return length
+        d={}
+        n=len(s)
+        for i in range(n):
+            d[s[i]]=d.get(s[i],0)+1
+        count=0
+        odd=0
+        for key,value in d.items():
+            if d[key]%2 == 0:
+                count+=d.get(key)
+            elif d[key]%2 !=0:
+                odd+=1
+                count+=d.get(key)
+            #odd
+        oddd=0
+        if odd!=0:
+            oddd=odd-1
+        return(count-oddd)
